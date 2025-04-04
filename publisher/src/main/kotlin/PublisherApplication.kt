22574175a0189eb847e189dd1c44e3628a41b921
ilanazare <ivanazare94@gmail.com>
2025-04-04 15:42:24 -0300

    enable caching

--- publisher/src/main/kotlin/PublisherApplication.kt
+++ publisher/src/main/kotlin/PublisherApplication.kt
@@ -2,8 +2,10 @@ package com.example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
+@EnableCaching
 class PublisherApplication
 fun main(args: Array<String>) {
     runApplication<PublisherApplication>(*args)
