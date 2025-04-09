e0be72b1813b566e48ab75459b7510508aec1fbb
ilanazare <ivanazare94@gmail.com>
2025-04-09 17:35:48 -0300

    adjusts connections applications with data sources

--- publisher/src/main/kotlin/controller/CustomerController.kt
+++ publisher/src/main/kotlin/controller/CustomerController.kt
@@ -3,8 +3,13 @@ package com.example.controller
 import com.example.entity.Customer
 import com.example.service.CustomerService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestBody
+import java.util.Optional
 
 
 @RestController
